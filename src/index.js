5513fe8e78a0426b52cd8f9943e52513c37dee9b
Roberto Achar <robertoachar@gmail.com>
2019-04-23 16:29:21 -0300

    feature: add mongodb

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+const mongoose = require('mongoose');
+
 const config = require('./config');
 const logger = require('./logger');
+
+mongoose.connect(config.DATABASE, { useNewUrlParser: true });
+
+mongoose.Promise = global.Promise;
+
+mongoose.connection.on('connected', () => {
+  logger.info('MongoDB connected!');
+});
+
+mongoose.connection.on('disconnected', () => {
+  logger.warn('MongoDB disconnected!');
+  process.exit(1);
+});
+
+mongoose.connection.on('error', (err) => {
+  logger.error('MongoDB Error!', err.message);
+  process.exit(1);
+});
 
 Object.keys(config).map((key) => logger.info(`${key}: ${config[key]}`));
