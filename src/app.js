e686f1274759ab09691535386e3f1a28c12e375a
Roberto Achar <robertoachar@gmail.com>
2019-04-23 16:33:40 -0300

    feature: add helmet

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const Koa = require('koa');
+const helmet = require('koa-helmet');
 
 const app = new Koa();
 
+app.use(helmet());
 app.use(async (ctx) => {
   ctx.body = { message: 'Hello Koa' };
 });
