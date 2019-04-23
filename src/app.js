c1b7eeb783d229b950f38ba8190606938e99203a
Roberto Achar <robertoachar@gmail.com>
2019-04-23 16:34:32 -0300

    feature: add body-parser

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const helmet = require('koa-helmet');
 
 const app = new Koa();
 
 app.use(helmet());
+app.use(bodyParser());
 app.use(async (ctx) => {
   ctx.body = { message: 'Hello Koa' };
 });
